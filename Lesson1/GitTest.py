1194b196fff2f4f45c6d263cf39c8562f29e2999
Dan Mikheenko <miheenko.denis@gmail.com>
2025-08-26 20:04:12 +0300

    Initial commit

---
+++ Lesson1/GitTest.py
@@ -0,0 +1,15 @@
+def bubble_sort(arr):
+    a = arr[:]
+    n = len(a)
+    for i in range(n - 1):
+        swapped = False
+        for j in range(0, n - 1 - i):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+                swapped = True
+        if not swapped:
+            break
+    return a
+
+data = [-12, 1, 45, 2, 8, 0]
+print(bubble_sort(data))
